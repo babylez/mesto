6b4ffca2d605d4d74e082a28eaa9df8a32bf3dda
Nikita Serbin <a1511782452@yandex.ru>
2021-04-22 00:18:18 +0300

    Mesto_first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 const openPopupButton = document.querySelector('.profile__edit');
-const popup = document.querySelector('.popup_is-opened');
+const popup = document.querySelector('.popup__is-opened');
 const closePopupButton = document.querySelector('.popup__close')
 
 function togglePopup(event) {
-  popup.classList.toggle('popup_is-opened');
+  popup.classList.toggle('popup__is-opened');
 };
 
 openPopupButton.addEventListener('click', togglePopup);
@@ -15,3 +15,19 @@ popup.addEventListener('click', function (event) {
     togglePopup(event);
   }
 })
+
+
+let popupButtonSave = document.querySelector('.popup__save');
+let profileName = document.querySelector('.profile__fullname');
+let profilejob = document.querySelector('.profile__job');
+
+function saveInfo() {
+  togglePopup();
+  let nameInput = document.querySelector('.popup__full-name').value;
+  profileName.innerHTML = nameInput;
+
+  let jobInput = document.querySelector('.popup__job').value;
+  profilejob.innerHTML = jobInput;
+}
+
+popupButtonSave.addEventListener('click', saveInfo);
